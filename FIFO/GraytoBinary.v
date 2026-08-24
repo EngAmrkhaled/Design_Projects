bd0f7df5057ab4b6e665529356c21ab46b6b44e6
EngAmrkhaled <amr.mohamed045@eng-st.cu.edu.eg>
2026-08-24 19:26:07 +0300

    FIFO: implement true depth-6 cyclic pointer

--- FIFO/GraytoBinary.v
+++ FIFO/GraytoBinary.v
@@ -1,12 +1,22 @@
 module GraytoBinary #(parameter WIDTH = 4) (
     input wire [WIDTH-1:0] G,
-    output wire [WIDTH-1:0] B
+    output reg  [WIDTH-1:0] B
 );
-    genvar i;
-    generate
-        assign B[WIDTH-1] = G[WIDTH-1];
-        for (i = WIDTH-2; i >= 0; i = i - 1) begin : gen_bin
-            assign B[i] = B[i+1] ^ G[i];
-        end
-    endgenerate
+    always @(*) begin
+        case (G)
+            4'b0000: B = 4'd0;
+            4'b0001: B = 4'd1;
+            4'b0011: B = 4'd2;
+            4'b0010: B = 4'd3;
+            4'b0110: B = 4'd4;
+            4'b0100: B = 4'd5;
+            4'b0101: B = 4'd6;
+            4'b0111: B = 4'd7;
+            4'b1111: B = 4'd8;
+            4'b1011: B = 4'd9;
+            4'b1001: B = 4'd10;
+            4'b1000: B = 4'd11;
+            default: B = 4'd0;
+        endcase
+    end
 endmodule
